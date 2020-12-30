c8471e987f27b54f135c8b5c64b711a04da88eec
hnkhanh <70878801+hnkhanh@users.noreply.github.com>
2020-12-30 22:30:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import './App.css';
 import marked from 'marked';
+import "./index.css";
 
 const initState = `
-# H1
 ## H2
 
 [I'm an inline-style link](https://www.google.com)
@@ -15,10 +14,12 @@ var s = "JavaScript syntax highlighting";
 alert(s);
 \`\`\`
 
+#### Ordered List
 1. First item
 2. Second item
 3. Third item
 
+#### Unordered List
 - First item
 - Second item
 - Third item
@@ -26,11 +27,9 @@ alert(s);
 
 > blockquotes are very handy in email to emulate reply text.
 
-Strong emphasis, aka bold, with **asterisks** or __underscores__.
+Strong emphasis, aka bold, with **asterisks** or __underscores__. 
 
-Here's a logo (hover to see the title text):
-
-![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+Here's a logo (hover to see the title text): ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
 
 `;
 
@@ -51,8 +50,8 @@ class App extends Component {
       <textarea id="editor" value={text} onChange={this.changeHandler} />
       <div id="preview" dangerouslySetInnerHTML={{__html: markdown}} />
     </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
